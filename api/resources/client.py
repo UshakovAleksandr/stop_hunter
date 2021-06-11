f9ce03941e62ea988714a4637f26c92a52c79b89
UshakovAleksandr <sashapimp@mail.ru>
2021-06-11 15:36:23 +0300

    add e

--- api/resources/client.py
+++ api/resources/client.py
@@ -6,44 +6,54 @@ from flask import request
 class ClientResponseResource(Resource):
 
     def get(self):
-        email = request.json["email"]
-        client = ClientModel.query.filter_by(email=email).first()
-        if not client:
-            abort(404, error=f"No client")
+        try:
+            email = request.json["email"]
+            client = ClientModel.query.filter_by(email=email).first()
+            if not client:
+                abort(404, error=f"No client")
+        except Exception as e:
+            return {"message": str(e)}, 400
         return client.to_dict(), 200
 
 
 class ClientResource(Resource):
 
     def get(self, client_id):
-        client = ClientModel.query.get(client_id)
-        if not client:
-            abort(404, error=f"No client with id={client_id}")
+        try:
+            client = ClientModel.query.get(client_id)
+            if not client:
+                return f"No client with id={client_id}", 404
+        except Exception as e:
+            return {"message": str(e)}, 400
         return client.to_dict(), 200
 
     def delete(self, client_id):
-        client = ClientModel.query.get(client_id)
-        if not client:
-            abort(404, error=f"No client with id={client_id}")
-        client.delete()
+        try:
+            client = ClientModel.query.get(client_id)
+            if not client:
+                return f"No client with id={client_id}", 404
+            client.delete()
+        except Exception as e:
+            return {"message": str(e)}, 400
         return f"User with id={client_id} deleted", 200
 
 
 class ClientResourceList(Resource):
 
     def get(self):
-        clients = ClientModel.query.all()
-        if not clients:
-            abort(404, error=f"No clients yet")
+        try:
+            clients = ClientModel.query.all()
+            if not clients:
+                return f"No clients yet", 404
+        except Exception as e:
+            return {"message": str(e)}, 400
         return [client.to_dict() for client in clients], 200
 
     @app.validate("client", "client")
     def post(self):
-        client = ClientModel(**request.json)
         try:
+            client = ClientModel(**request.json)
             client.save()
             return client.to_dict(), 201
-        except:
-            abort(404, error=f"An error occurred while adding new client" \
-                             "or a client with such email is already exist. " \
-                             "You can only add a client with unique email")
+        except Exception as e:
+            return {"message": str(e)}, 400
